38bce2c4cc1932502d95d55a7419efcf39a12985
Miseon Im <soup7912@gmail.com>
2025-04-04 11:14:25 +0900

    feat: icons

---
+++ src/app/generated/icons.ts
@@ -0,0 +1,81 @@
+import Aperture from "@/app/icons/Aperture.svg";
+import ArrowRight from "@/app/icons/ArrowRight.svg";
+import ArrowsOutCardinal from "@/app/icons/ArrowsOutCardinal.svg";
+import ArrowSquareUpRight from "@/app/icons/ArrowSquareUpRight.svg";
+import ArrowUp from "@/app/icons/ArrowUp.svg";
+import BezierCurve from "@/app/icons/BezierCurve.svg";
+import CaretDown from "@/app/icons/CaretDown.svg";
+import Check from "@/app/icons/Check.svg";
+import Cube from "@/app/icons/Cube.svg";
+import CubeFocus from "@/app/icons/CubeFocus.svg";
+import Cursor from "@/app/icons/Cursor.svg";
+import Dimension from "@/app/icons/Dimension.svg";
+import DotsThree from "@/app/icons/DotsThree.svg";
+import DWG from "@/app/icons/DWG.svg";
+import Export from "@/app/icons/Export.svg";
+import FilePlus from "@/app/icons/FilePlus.svg";
+import FilePng from "@/app/icons/FilePng.svg";
+import Gear from "@/app/icons/Gear.svg";
+import House from "@/app/icons/House.svg";
+import MagnifyingGlassMinus from "@/app/icons/MagnifyingGlassMinus.svg";
+import MagnifyingGlassPlus from "@/app/icons/MagnifyingGlassPlus.svg";
+import Moon from "@/app/icons/Moon.svg";
+import Pause from "@/app/icons/Pause.svg";
+import PDF from "@/app/icons/PDF.svg";
+import PencilSimple from "@/app/icons/PencilSimple.svg";
+import Plus from "@/app/icons/Plus.svg";
+import Retry from "@/app/icons/Retry.svg";
+import SidebarSimple from "@/app/icons/SidebarSimple.svg";
+import SignOut from "@/app/icons/SignOut.svg";
+import SkipBack from "@/app/icons/SkipBack.svg";
+import SkipForward from "@/app/icons/SkipForward.svg";
+import Sphere from "@/app/icons/Sphere.svg";
+import Spinner from "@/app/icons/Spinner.svg";
+import Sun from "@/app/icons/Sun.svg";
+import Trash from "@/app/icons/Trash.svg";
+import Triangle from "@/app/icons/Triangle.svg";
+import Upload from "@/app/icons/Upload.svg";
+import VolumeBox from "@/app/icons/VolumeBox.svg";
+import X from "@/app/icons/X.svg";
+
+export const ICONS = {
+  Aperture: Aperture,
+  ArrowRight: ArrowRight,
+  ArrowsOutCardinal: ArrowsOutCardinal,
+  ArrowSquareUpRight: ArrowSquareUpRight,
+  ArrowUp: ArrowUp,
+  BezierCurve: BezierCurve,
+  CaretDown: CaretDown,
+  Check: Check,
+  Cube: Cube,
+  CubeFocus: CubeFocus,
+  Cursor: Cursor,
+  Dimension: Dimension,
+  DotsThree: DotsThree,
+  DWG: DWG,
+  Export: Export,
+  FilePlus: FilePlus,
+  FilePng: FilePng,
+  Gear: Gear,
+  House: House,
+  MagnifyingGlassMinus: MagnifyingGlassMinus,
+  MagnifyingGlassPlus: MagnifyingGlassPlus,
+  Moon: Moon,
+  Pause: Pause,
+  PDF: PDF,
+  PencilSimple: PencilSimple,
+  Plus: Plus,
+  Retry: Retry,
+  SidebarSimple: SidebarSimple,
+  SignOut: SignOut,
+  SkipBack: SkipBack,
+  SkipForward: SkipForward,
+  Sphere: Sphere,
+  Spinner: Spinner,
+  Sun: Sun,
+  Trash: Trash,
+  Triangle: Triangle,
+  Upload: Upload,
+  VolumeBox: VolumeBox,
+  X: X,
+};
